40e0076fe360143cc6066cbab5f897fd9c5445a8
r4jiv007 <r4jiv007@gmail.com>
2017-03-27 11:20:36 +0400

    ignoring 9-patch image

--- makeItTiny.py
+++ makeItTiny.py
@@ -17,7 +17,7 @@ fileList=os.listdir(path);
 
 print "compressing following files :- \n"
 for filename in fileList:
-    if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
+    if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg') and '.9.' not in filename:
         print "processing "+ filename+'\n'
         source = tinify.from_file(os.path.join(path,filename))
         source.to_file(os.path.join(path,filename))
