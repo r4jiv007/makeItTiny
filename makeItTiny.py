9a16b22101f7a85a9e94aba7e2f3ca7cc2ba4c5b
r4jiv007 <r4jiv007@gmail.com>
2017-10-03 11:27:50 +0400

    added functionality for crawling sub-directory and processing imagefiles

--- makeItTiny.py
+++ makeItTiny.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 import tinify
 import os
 import sys
 
 
-tinify.key = "GsTvovEirnd4ru8LcPGzawVmmrY—VZn"
+tinify.key ="enter your api key here"
 
 
 path = raw_input("Enter directory address: ");
@@ -13,14 +14,21 @@ if not path:
     sys.exit()
 
 
-fileList=os.listdir(path);
+# fileList=os.listdir(path);
+filePath =[]
+
+for folder, subs, files in os.walk(path):
+  for filename in files:
+    filePath.append(os.path.abspath(os.path.join(folder, filename)))
+
+# print filePath
 
 print "compressing following files :- \n"
-for filename in fileList:
+for filename in filePath:
     if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg') :
         if  not  filename.endswith('.9.png'):
             print "processing "+ filename+'\n'
-            source = tinify.from_file(os.path.join(path,filename))
-            source.to_file(os.path.join(path,filename))
+            source = tinify.from_file(filename)
+            source.to_file(filename)
 
 print "processing finished...."
